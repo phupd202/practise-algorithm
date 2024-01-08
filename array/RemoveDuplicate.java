16cb9200a679f5c7bddb4d1f6a3ff5978948fc80
phupd202 <phutct855@gmail.com>
2024-01-08 12:10:10 +0700

    East leetcode: Remove duplicate with in-place method

--- array/RemoveDuplicate.java
+++ array/RemoveDuplicate.java
@@ -9,6 +9,8 @@
  * Required: Using in-place method. 
  */
 
+import java.util.Arrays;
+
 public class RemoveDuplicate {
     public static int removeDuplicates(int[] nums) {
         int currentPos = 0;
@@ -29,15 +31,17 @@ public class RemoveDuplicate {
         return realLength + 1;
     }
 
-    public static void main(String[] args) {
-        int nums[] = {0,0,1,1,1,2,2,3,3,4};
-
-        int k = removeDuplicates(nums);
-        System.out.println(k);
-
-        for (int i = 0; i < k; i++) {
-            System.out.print(nums[i] + " ");
-            
+    public static void printFrom(int[] nums, int begin, int finish) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " " );
         }
+        
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6};
+        int k1 = removeDuplicates(nums);
+        System.out.println(k1);
+        System.out.println(Arrays.toString(Arrays.copyOfRange(nums, 0, k1)));
     }
 }
