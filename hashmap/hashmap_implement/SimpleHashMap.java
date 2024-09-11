e6c8d52ecd7d7304c664b26da836c3fa7be2275c
phupd202 <phutct855@gmail.com>
2024-09-11 23:30:21 +0700

    110924: Try implement hashmap follow Collection Framework of Java

---
+++ hashmap/hashmap_implement/SimpleHashMap.java
@@ -0,0 +1,132 @@
+package hashmap.hashmap_implement;
+
+import java.util.Set;
+
+/*
+ * Implement a simple hashmp
+ * Using 2 same size array
+ */
+public class SimpleHashMap<K, V> implements SimpleMapInterface<K, V>{
+    private K[] keys;
+    private V[] values;
+    private int n = 0; // count item already existed in the map
+    private int defaultSize;
+
+    // constructor
+    public SimpleHashMap() {
+        this.keys = (K[]) new Object[defaultSize];
+        this.values = (V[]) new Object[defaultSize];
+    }
+
+    // constructor
+    public K[] getKeys() {
+        return keys;
+    }
+
+    public void setKeys(K[] keys) {
+        this.keys = keys;
+    }
+
+    public V[] getValues() {
+        return values;
+    }
+
+    public void setValues(V[] values) {
+        this.values = values;
+    }
+
+    @Override
+    /*
+     * Complexity of operation build map: O(n^2) - because using 2 loop
+     * Decrease complexity: Using 2 sorted array --> 
+     * 
+     */
+    public Boolean put(K key, V value) {
+        // if empty --> dont need check existed
+        if(this.isEmpty()) {
+            keys[n] = key;
+            values[n] = value;
+            n++;
+            return true;
+        } else {
+            // check existed
+            for (int i = 0; i < this.size(); i++) {
+                if(keys[i].equals(key)) {
+                    return false;
+                }
+            }
+
+            // if not existed --> insert
+            if(this.size() >= defaultSize) {
+                // scale array if overflow
+                K[] newKeys = (K[]) new Object[defaultSize * 2];
+                V[] newValues = (V[]) new Object[defaultSize * 2];
+
+                // copy old item into new array
+                System.arraycopy(keys, 0, newKeys, 0, defaultSize);
+                System.arraycopy(values, 0, newValues, 0, defaultSize);
+                this.keys = newKeys;
+                this.values = newValues;
+            }
+            keys[n] = key;
+            values[n] = value;
+            n++;
+            return true;
+        }
+    }
+
+    private Boolean checkExistedItem(K[] array, K item, int begin, int end) {
+        for (int i = begin; i < end; i++) {
+            if(array[i].equals(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean putEntry(Entry<K, V> entry) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'putEntry'");
+    }
+
+    @Override
+    public Boolean putAll(SimpleMapInterface<K, V> map) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'putAll'");
+    }
+
+    @Override
+    public Boolean remove(K key, V value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public Boolean removeAll(SimpleMapInterface<K, V> map) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeAll'");
+    }
+
+    @Override
+    public Integer size() {
+        return n;
+    }
+
+    @Override
+    public Boolean isEmpty() {
+        return n == 0;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'entrySet'");
+    }
+
+    @Override
+    public Set<K> keySet() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keySet'");
+    } 
+}
---
+++ hashmap/hashmap_implement/SimpleMapInterface.java
@@ -0,0 +1,36 @@
+package hashmap.hashmap_implement;
+
+import java.util.List;
+/*
+ * Build a simple map follow to Map (Collections Framework - Java)
+ */
+import java.util.Set;
+
+public interface SimpleMapInterface<K, V> {
+    // put with key and value
+    Boolean put(K key, V value);
+
+    // put entry
+    Boolean putEntry(Entry<K, V> entry);
+
+    // put a map into a map
+    Boolean putAll(SimpleMapInterface<K, V> map);
+
+    // remove a item from the map
+    Boolean remove(K key, V value);
+
+    // remove a submap from the map
+    Boolean removeAll(SimpleMapInterface<K, V> map);
+
+    // return size of the map
+    Integer size();
+
+    // check map is empty
+    Boolean isEmpty();
+
+    // return set of entry
+    Set<Entry<K, V>> entrySet();
+
+    // return set of key
+    Set<K> keySet();
+}
